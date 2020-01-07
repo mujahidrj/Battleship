32a9e6bfa4ff7459f373736eb9567caeb7e0caaa
Mujahid Jaffer <mujahidrj@gmail.com>
2020-01-07 09:48:26 -0500

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -1,13 +1,14 @@
 let isSunk = false;
 let guesses = 0;
 let hits = 0;
-let location1 = 3,
-	location2 = 4,
-	location3 = 5;
+let randomLocation = Math.floor(Math.random() * 5);
+let location1 = randomLocation,
+	location2 = location1 + 1,
+	location3 = location2 + 1;
 let guess;
-while (isSunk === false) {
+while (!isSunk) {
 	guess = prompt('Ready, aim, fire! Enter a number from 0-6');
-	if (guess < 0 || guess > 6) guess = prompt('Enter a valid number');
+	if (guess < 0 || guess > 6) alert('Enter a valid number');
 	else {
 		guesses++;
 		if (guess == location1 || guess == location2 || guess == location3) {
